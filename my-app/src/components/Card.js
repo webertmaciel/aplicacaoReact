9b226924f239339c6de6fb79e384340690697d12
webert maciel <webert_maciel@hotmail.com>
2021-12-01 18:00:32 -0300

    a

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 function Card(props) {
-    return <div className={props.className ? `${props.className}card` : "card"}>{props.children}</div>;
+    return <div className={props.className ? `${props.className}card ` : "card"}>{props.children}</div>;
 }
 
 
